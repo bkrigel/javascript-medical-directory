7d555ba479b67e246edfc4968ad6f5d152d0e555
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-09-13 22:44:02 -0400

    added specs for Appointment model

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  
+
   describe "Appointment model" do
 
     before (:each) do
@@ -26,12 +26,39 @@ RSpec.describe Appointment, type: :model do
       )
     end
 
-    it "belongs to an Ailment" do
-
+    it "has a valid date and time" do
+      valid_appointment = Appointment.new(
+        scheduled_for: DateTime.now,
+        duration_in_minutes: 70,
+        ailment_id: @test_ailment.id,
+        patient_id: @test_patient.role.id,
+        doctor_id: @test_doctor.role.id
+      )
+      invalid_appointment = Appointment.new(
+        scheduled_for: "today",
+        duration_in_minutes: 70,
+        ailment_id: @test_ailment.id,
+        patient_id: @test_patient.role.id,
+        doctor_id: @test_doctor.role.id
+      )
+      expect(invalid_appointment).to be_invalid
+      expect(valid_appointment).to be_valid
     end
 
-    it "must have a scheduled time and duration" do
+    it "has a valid duration" do
+      valid_appointment = Appointment.new(
+        scheduled_for: DateTime.now,
+        duration_in_minutes: "thirty minutes",
+        ailment_id: @test_ailment.id,
+        patient_id: @test_patient.role.id,
+        doctor_id: @test_doctor.role.id
+      )
+      expect(valid_appointment).to be_invalid
+    end
 
+    it "belongs to an Ailment" do
+      v = Appointment.reflect_on_association(:ailment)
+      expect(v.macro).to eq(:belongs_to)
     end
 
   end
